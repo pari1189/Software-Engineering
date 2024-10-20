c92c065de150a5441e39a3917ee4a56cb26d2f68
pari1189 <155975494+pari1189@users.noreply.github.com>
2024-10-20 12:55:29 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import RegisterUserForm from './RegisterUserForm';
+import StaffGrid from './StaffGrid';
+
+const App = () => {
+  return (
+    <div>
+      <RegisterUserForm />
+      <StaffGrid />
+    </div>
+  );
+};
+
+export default App;
